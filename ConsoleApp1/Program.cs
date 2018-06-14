a8210ad306a9e94f9bb4824f17f2071a151b9e40
NetSeed <net_seed@hotmail.com>
2018-06-14 23:59:54 +0900

    On going...

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+	        var list = new List<(int x, int y)>();
+
+	        IEnumerable<(int, int)> hoge = list;
+
+        }
+    }
+}
---
+++ FPS.CoreLib/Parser/FixedPoint.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Parseq;
+
+namespace FPS.CoreLib.Parser
+{
+	internal class FixedPoint<TToken, T>
+	{
+		public readonly string Name;
+		private Parser<TToken, T> _fixedParser;
+
+		public FixedPoint(string name)
+		{
+			Name = name ?? "";
+		}
+
+		public Parser<TToken, T> FixedParser
+		{
+			set
+			{
+				if (_fixedParser != null) throw new InvalidOperationException("Parser is already registered.");
+				_fixedParser = value ?? throw new NullReferenceException();
+			}
+		}
+
+		public IReply<TToken, T> Parse(ITokenStream<TToken> stream)
+		{
+			if (_fixedParser == null) throw new InvalidOperationException("Parser isn't registered.");
+
+
+			var line = stream.Current.HasValue ? stream.Current.Value.Item1.Line : -1;
+			var column = stream.Current.HasValue ? stream.Current.Value.Item1.Column : -1;
+
+			//Console.WriteLine($"{Name} is Called. Line:{line} Col:{column} Token:{stream.Current.Value.Item0}");
+
+
+			return _fixedParser(stream);
+		}
+	}
+}
