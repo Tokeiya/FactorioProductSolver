3b9b2d73f92f56e8335fa942dfe5907be85704a3
NetSeed <net_seed@hotmail.com>
2018-06-15 18:15:34 +0900

    Elementとりあえず作ってみた。

---
+++ FPS.CoreLib/Parser/Element.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FPS.CoreLib.Parser
+{
+	public enum ElementTypes
+	{
+		Table=1,
+		Value
+	}
+
+	public abstract class Element
+	{
+		protected Element(ElementTypes elementType)
+		{
+			Type=elementType;
+		}
+
+
+		public ElementTypes Type{get;}
+	}
+
+	public class TableElement:Element
+	{
+		public TableElement():base(ElementTypes.Table)
+		{
+			
+		}
+
+	}
+
+
+}
