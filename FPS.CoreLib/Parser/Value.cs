500aa5f5ac0343aebf8d4ea8dd753bfdf853a9d2
NetSeed <net_seed@hotmail.com>
2018-06-15 21:38:56 +0900

    Value.Createメソッド追加

--- FPS.CoreLib/Parser/Value.cs
+++ FPS.CoreLib/Parser/Value.cs
@@ -4,6 +4,26 @@ namespace FPS.CoreLib.Parser
 {
 	public abstract class Value
 	{
+		public static Value Create(Token token)
+		{
+			switch (token.TokenType)
+			{
+				case TokenTypes.BooleanLiteral:
+					return new BooleanValue(bool.Parse(token.Value));
+
+				case TokenTypes.IntegerLiteral:
+					return new IntegerValue(int.Parse(token.Value));
+
+				case TokenTypes.StringLiteral:
+					return new TextValue(token.Value);
+
+				case TokenTypes.RealLiteral:
+					return new RealValue(double.Parse(token.Value));
+				default:
+					throw new InconsistencyException();
+			}
+		}
+
 		protected Value(ValueTypes valueType)
 		{
 			if (!valueType.Verify()) throw new ArgumentException($"{valueType} is unexpected.");
