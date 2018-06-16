eb3eb5b5aa37659b8b24a6e4b2de4eab680d4545
NetSeed <net_seed@hotmail.com>
2018-06-17 00:33:32 +0900

    要素を検出。

--- TestBench/Program.cs
+++ TestBench/Program.cs
@@ -1,35 +1,39 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using FPS.CoreLib.Parser;
 using Parseq;
-using Parseq.Combinators;
 
-namespace ConsoleApp1
+namespace TestBench
 {
 	internal class Program
 	{
 		private static void Main(string[] args)
 		{
+			var dict = new Dictionary<string, int>();
 
-			NewMethod();
-			Console.ReadLine();
-		}
+			var elements = Directory.GetFiles(".\\SampleRecipe")
+				.Select(p => ElementParser.RecipeParser(File.ReadAllText(p).AsStream()))
+				.Select(x => x.Case((_, __) => throw new Exception(), (_, elem) => elem))
+				.SelectMany(x => x.Traverse()).ToArray();
 
-		private static void NewMethod()
-		{
-			var recipe = File.ReadAllText("..\\..\\..\\..\\SampleRecipe\\recipe.lua");
-
-
-			var ret = ElementParser.RecipeParser(recipe.AsStream());
-
-			ret.Case((stream, msg) =>
+			foreach (var element in elements.Where(x=>x.Identifier!=""))
 			{
-				var pos = stream.Current.Value.Item1;
+				if (dict.TryGetValue(element.Identifier, out int cnt))
+				{
+					dict[element.Identifier] = ++cnt;
+				}
+				else
+				{
+					dict.Add(element.Identifier, 1);
+				}
+			}
 
-				Console.WriteLine($"Line:{pos.Line},Col:{pos.Column}");
+			File.WriteAllLines("G:\\output.txt", dict.Select(pair => $"{pair.Key}\t{pair.Value}"));
 
-				Console.WriteLine("fail");
-			}, (_, elem) => { Console.WriteLine("success"); });
+
 		}
+
 	}
 }
