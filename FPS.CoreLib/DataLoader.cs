730be68c1449a2fe33b7b7f4e76267cda201b86d
NetSeed <net_seed@hotmail.com>
2018-06-17 20:15:51 +0900

    ローダーを実装 パース失敗時の例外を作成。

---
+++ FPS.CoreLib/DataLoader.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using FPS.CoreLib.Entity;
+using FPS.CoreLib.Parser;
+using Parseq;
+
+namespace FPS.CoreLib
+{
+	public static class DataLoader
+	{
+		public static TableElement Load(string path)
+		{
+			var source = File.ReadAllText(path ?? throw new ArgumentNullException(nameof(path))).AsStream();
+
+			var rep = LuaTableParser.RecipeParser(source);
+			return rep.Case((str, _) => throw new ParseException(str.Current), (_, elem) => elem);
+		}
+	}
+}
---
+++ FPS.CoreLib/ParseException.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Parseq;
+
+namespace FPS.CoreLib
+{
+	public class ParseException:Exception
+	{
+		public ParseException(IOption<IPair<char,Position>> current)
+		{
+			if (current.HasValue)
+			{
+				ExceptionOccuredChar = current.Value.Item0;
+
+				Line = current.Value.Item1.Line;
+				Column = current.Value.Item1.Column;
+			}
+		}
+
+		public char? ExceptionOccuredChar { get; }
+
+		public int? Line { get; }
+		public int? Column { get; }
+
+
+
+		public override string Message =>
+			$"Parse error...Line:{Line ?? -1} Column:{Column ?? -1} Char:{ExceptionOccuredChar?.ToString() ?? "Unknown"}";
+	}
+}
