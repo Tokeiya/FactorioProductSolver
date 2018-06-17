a65d46e1a80baf3f584921c5f5ef95e85570cebe
NetSeed <net_seed@hotmail.com>
2018-06-17 20:26:32 +0900

    ValueElement.GetChildが要素0を返すように変更

--- FPS.CoreLib/Entity/ValueElement.cs
+++ FPS.CoreLib/Entity/ValueElement.cs
@@ -14,10 +14,7 @@ namespace FPS.CoreLib.Entity
 		public Value Content { get; }
 
 
-		public override IEnumerable<Element> GedChildren()
-		{
-			throw new NotSupportedException();
-		}
+		public override IEnumerable<Element> GedChildren() => Array.Empty<Element>();
 
 		public override IEnumerable<Element> Traverse()
 		{
