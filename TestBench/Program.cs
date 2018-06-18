14620cddde77496f7cc31c255c923fdcaf99e9b9
NetSeed <net_seed@hotmail.com>
2018-06-18 18:00:25 +0900

    TestBench was cleanuped.

--- TestBench/Program.cs
+++ TestBench/Program.cs
@@ -1,33 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using FPS.CoreLib.Entity;
-using FPS.CoreLib.Parser;
-using Parseq;
-
-namespace TestBench
+﻿namespace TestBench
 {
+
+	//https://wiki.factorio.com/Prototype/Recipe#Modified_so_that_the_expensive_recipe_is_always_used.2C_even_in_normal_mode
+
 	internal class Program
 	{
 		private static void Main(string[] args)
 		{
-			var elements = Directory.GetFiles(".\\SampleRecipe")
-				.Select(p => LuaTableParser.RecipeParser(File.ReadAllText(p).AsStream()))
-				.Select(x => x.Case((_, __) => throw new Exception(), (_, elem) => elem))
-				.SelectMany(x => x.GedChildren()).ToArray();
-
-			var hoge=elements.Where(x => x.GedChildren().OfType<TableElement>().Any(elem => elem.Identifier == "name"))
-				.Select(elem => elem.GedChildren().Where(x => x.Identifier == "name").First())
-				.ToArray();
-
-			foreach (var element in hoge.Select(x=>((ValueElement)x).Content.ValueAsObject))
-			{
-				Console.WriteLine(element);
-			}
-
-
 		}
-
 	}
 }
